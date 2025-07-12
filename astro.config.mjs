7cfa371a5e2e39dd382b648c749e46492770159b
lemon <git@unseen.ninja>
2025-07-12 18:50:06 +0200

    feat(layout): move to NPM; use starlight-obsidian
    starlight-obsidian seems to not play super nice with my local pnpm version. hence i'm switching this to npm.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeObsidian from 'starlight-theme-obsidian';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
+			// pagefind: false,
 			title: 'Dungeons & Dragons',
-			// social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/unseen-ninja' }],
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/unseen-ninja/'
+				},
+				{
+					icon: 'information',
+					label: 'Website',
+					href: 'https://unseen.ninja/'
+				}
+			],
 			sidebar: [
 				{
 					label: 'Hofer',
@@ -20,6 +33,12 @@ export default defineConfig({
 			],
 			customCss: [
 				'./src/styles/custom.css',
+			],
+			plugins: [
+				starlightThemeObsidian({
+					backlinks: false,
+					graph: false
+				})
 			],
 		}),
 	],
